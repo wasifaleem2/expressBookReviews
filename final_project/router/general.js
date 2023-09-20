208c0f2102f29dccec2be5f56e310d9f9f4b3063
wasifaleem2 <wasifaleem2@gmail.com>
2023-09-20 07:17:14 -0400

    feat: api t o fetch books with title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -40,15 +40,17 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-   
+    let title = req.params.title;
+    let booksArray = Object.values(books);
+    let filterbooks = booksArray.filter((bk)=> {
+        return bk.title == title
+    });
+    return res.status(300).json({message: `books with the author ${title}`, books: filterbooks});   
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
 });
 
 module.exports.general = public_users;
